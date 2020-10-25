104af9aafe57ad51308e3d94c903eac826138433
Pavel <pavelgric@gmail.com>
2020-10-25 13:28:42 +0100

    link example

--- src/test.tsx
+++ src/test.tsx
@@ -1,24 +1,24 @@
 import { Text, View, ViewStyle, StyleSheet } from "react-native";
-import React from "react";
+import * as React from "react";
 
 type TestProps = {
-	style?: ViewStyle;
+  style?: ViewStyle;
 };
 
 const Test = (props: TestProps) => {
-	return (
-		<View style={[styles.container, props.style]}>
-			<Text>Test Component</Text>
-		</View>
-	);
+  return (
+    <View style={[styles.container, props.style]}>
+      <Text>Test Component</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		justifyContent: "center",
-		alignItems: "center",
-		flex: 1,
-	},
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
 });
 
 export { Test };
