73b852e5d7a6c37c705806104c97e3366aae631f
Pavel <pavelgric@gmail.com>
2021-02-16 12:37:50 +0100

    more typography

--- example/src/themes/variants/themeCreator.ts
+++ example/src/themes/variants/themeCreator.ts
@@ -1,3 +1,4 @@
+import { StyleSheet } from 'react-native';
 import { borderRadii, spacing } from '../res/dims';
 import { fontSizes } from '../res/fonts';
 
@@ -24,60 +25,87 @@ export const createTheme = (props: Props) => ({
   borderRadii,
 });
 
-const createTypography = ({ colors }: Props) => ({
-  h1: {
-    fontSize: fontSizes.h1,
-    color: colors.primary,
-  },
-  h2: {
-    fontSize: fontSizes.h2,
-    color: colors.primary,
-  },
-  h3: {
-    fontSize: fontSizes.h3,
-    color: colors.primary,
-  },
-  h4: {
-    fontSize: fontSizes.h4,
-    color: colors.primary,
-  },
-  h5: {
-    fontSize: fontSizes.h5,
-    color: colors.primary,
-  },
-  h6: {
-    fontSize: fontSizes.h6,
-    color: colors.primary,
-  },
-  subtitle1: {
-    fontSize: fontSizes.large,
-    color: colors.secondary,
-  },
-  subtitle2: {
-    fontSize: fontSizes.medium,
-    color: colors.secondary,
-  },
-  body1: {
-    fontSize: fontSizes.large,
-    color: colors.primary,
-  },
-  body2: {
-    fontSize: fontSizes.medium,
-    color: colors.secondary,
-  },
-  button: {
-    fontSize: fontSizes.medium,
-    color: colors.onPrimary,
-  },
-  caption: {
-    fontSize: fontSizes.small,
-    color: colors.primary,
-  },
-  overline: {
-    fontSize: fontSizes.tiny,
-    color: colors.primary,
-  },
-});
+const createTypography = ({ colors }: Props) =>
+  StyleSheet.create({
+    h1: {
+      fontSize: fontSizes.h1,
+      color: colors.primary,
+      letterSpacing: -1.5,
+      fontWeight: '300',
+    },
+    h2: {
+      fontSize: fontSizes.h2,
+      color: colors.primary,
+      letterSpacing: -0.5,
+      fontWeight: '300',
+    },
+    h3: {
+      fontSize: fontSizes.h3,
+      color: colors.primary,
+      letterSpacing: 0,
+      fontWeight: '400',
+    },
+    h4: {
+      fontSize: fontSizes.h4,
+      color: colors.primary,
+      letterSpacing: 0.25,
+      fontWeight: '400',
+    },
+    h5: {
+      fontSize: fontSizes.h5,
+      color: colors.primary,
+      letterSpacing: 0,
+      fontWeight: '400',
+    },
+    h6: {
+      fontSize: fontSizes.h6,
+      color: colors.primary,
+      letterSpacing: 0.15,
+      fontWeight: '500',
+    },
+    subtitle1: {
+      fontSize: fontSizes.large,
+      color: colors.secondary,
+      letterSpacing: 0.15,
+      fontWeight: '400',
+    },
+    subtitle2: {
+      fontSize: fontSizes.medium,
+      color: colors.secondary,
+      letterSpacing: 0.1,
+      fontWeight: '500',
+    },
+    body1: {
+      fontSize: fontSizes.large,
+      color: colors.primary,
+      letterSpacing: 0.5,
+      fontWeight: '500',
+    },
+    body2: {
+      fontSize: fontSizes.medium,
+      color: colors.secondary,
+      letterSpacing: 0.25,
+      fontWeight: '500',
+    },
+    button: {
+      fontSize: fontSizes.medium,
+      color: colors.onPrimary,
+      letterSpacing: 1.25,
+      fontWeight: '500',
+    },
+    caption: {
+      fontSize: fontSizes.small,
+      color: colors.primary,
+      letterSpacing: 0.4,
+      fontWeight: '500',
+    },
+    overline: {
+      fontSize: fontSizes.tiny,
+      color: colors.primary,
+      letterSpacing: 1.5,
+      fontWeight: '500',
+    },
+  });
 
 export type Theme = ReturnType<typeof createTheme>;
 export type Typography = ReturnType<typeof createTypography>;
