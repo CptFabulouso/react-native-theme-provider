2273442247ce3f569cdc187a9b70a5391393ee16
Pavel Gric <pavelgric@gmail.com>
2022-05-31 15:18:45 +0200

    Reflect system theme in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useColorScheme } from 'react-native';
 
 import HomeScreen from './screens/Home/HomeScreen';
 import { ThemeProvider } from '@themes';
 
 const App = () => {
+  const colorScheme = useColorScheme();
+
   return (
-    <ThemeProvider>
+    <ThemeProvider initialTheme={colorScheme ?? 'light'}>
       <HomeScreen />
     </ThemeProvider>
   );
